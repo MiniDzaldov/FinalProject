d57e2473afde489ae905a8bfd40078c1239f0520
וייג שרה < @mbjcomp.local>
2024-02-13 16:07:46 +0200

    testing the project

---
+++ ServerSideCode/DAL/DALApi/IAssistRepo.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DALApi;
+
+public interface IAssistRepo:IRepository<Assist>
+{
+   /* public Task<List<Assist>> GetAllAsync();
+    public Task<Assist> GetAsync(int id);
+    public Task<Assist> PutAsync(int id, Assist assist);
+    public Task<Assist> PostAsync(Assist assist);
+    public Task<Assist> DeleteAsync(int id);*/
+}
