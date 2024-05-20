df5a22af8f3af106b3a88da7e7814985314302dd
וייג שרה < @mbjcomp.local>
2024-05-20 15:23:05 +0300

    cleaning  test data...

--- ServerSideCode/BLL/BLLImplementation/AssistantService.cs
+++ ServerSideCode/BLL/BLLImplementation/AssistantService.cs
@@ -15,19 +15,11 @@ public class AssistantService : IAssistantService
     {
         var assistantList = await assistantRepo.GetAllAsync();
         var assistantDetailList = new List<AssistantDTO>();
-        foreach(var ad in assistantList)
+        foreach (var ad in assistantList)
         {
-            assistantDetailList.Add(new AssistantDTO(ad.FirstName , ad.LastName, ad.PhoneNumber, ad.Email));
+            assistantDetailList.Add(new AssistantDTO(ad.FirstName, ad.LastName, ad.PhoneNumber, ad.Email));
         }
         assistantDetailList.Add(new AssistantDTO("Chana", "Levi", "0548744154", "c@gmail.com"));
         return assistantDetailList;
     }
-<<<<<<< HEAD
-=======
-
-  
-
-
-
->>>>>>> bd5f45e4ec1aea002ac1ad1a5ba226ebb222d405
 }
